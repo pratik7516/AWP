becff54a5507a15b8c488d384608b3885e33d18e
pratik7516 <68392138+pratik7516@users.noreply.github.com>
2020-08-14 03:09:27 -0700

    Add files via upload

---
+++ Day8.1.js
@@ -0,0 +1,18 @@
+function pratik(fn) {
+    fn();
+}
+
+
+pratik(function () { })   //simple function0
+
+pratik(() => { })     //arrow function
+
+let aditya function() { };
+
+pratik(aditya)
+
+let anfn = () => { };
+pratik(anfn);
+
+
+
